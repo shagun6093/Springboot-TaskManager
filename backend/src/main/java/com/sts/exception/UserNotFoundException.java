04d5826dd5ba5651bac25f86ad9b53d525ca7568
shagun6093 <shagun6093@gmail.com>
2024-01-21 10:51:06 +0530

    backend added

---
+++ backend/src/main/java/com/sts/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package com.sts.exception;
+
+
+
+	public class UserNotFoundException extends RuntimeException{
+	    public UserNotFoundException(Long id){
+	        super("Could not found the user with id "+ id);
+	    }
+	}
